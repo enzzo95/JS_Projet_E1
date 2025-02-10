b76663f7447dab892706ce0227df0b4c3adc69f4
Enzo a <soler.enzo@gmail.com>
2025-02-10 22:52:06 +0100

    fonction edit et annotations

--- app.js
+++ app.js
@@ -1,48 +1,64 @@
+// Constante du bouton créer
 const button_add = document.getElementById("add")
-const check = document.getElementById("checkbox")
 
+// Actions si le bouton créer est cliqué
 button_add.addEventListener("click", function() {
-    createDiv()
-    update_check()
+
+    // Si le contenu de la tache n'est pas vide
+    if (document.getElementById("name").value)
+    {
+        createDiv()
+        update_check()
+    }
 })
 
+// Fonction créer une tache
 function createDiv() {
-    // Créer le div
-    const div = document.createElement("div");
 
-    // Créer l'élément li
+    // Créer le div et li
+    const div = document.createElement("div");
     const li = document.createElement("li");
 
-    // Récupérer la valeur du formulaire
+    // Récupérer valeur du form (texte de la tache)
     const name = document.getElementById("name").value;
 
-    // Lier à li
+    // Ajout valeur du form dans le li
     li.textContent = name;
 
-    // Création de la checkbox
+    // Création checkbox + eventListener
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.id = "checkbox";
-
     checkbox.addEventListener("change", function() {
         update_check()
     })
 
-    // Création du bouton de suppression
-    const button = document.createElement("button");
-    button.textContent = "Supprimer";
-    button.addEventListener("click", function() {
+    // Création bouton de suppression + eventListener
+    const button_delete = document.createElement("button");
+    button_delete.textContent = "Supprimer";
+    button_delete.addEventListener("click", function() {
         div.remove();
         update_check();
     });
 
+    // Création bouton edit + eventListener
+    const button_edit = document.createElement("button");
+    button_edit.textContent = "Modifier"
+    button_edit.addEventListener("click", function() {
+        edit(div);
+    });
+
+    // Ajout des éléments au div
     div.appendChild(checkbox);
     div.appendChild(li);
-    div.appendChild(button);
+    div.appendChild(button_edit);
+    div.appendChild(button_delete);
 
+    // Ajout du div dans le ul "liste"
     document.getElementById("liste").appendChild(div);
 }
 
+// Fonction compter taches faites/totale
 function update_check()
 {
     const liste = document.querySelectorAll("ul > div > input")
@@ -51,6 +67,7 @@ function update_check()
     let compteur = 0
     let max = 0
 
+    // Compte le nombre d'elements total et checkList coché
     liste.forEach(items => {
         max += 1
 
@@ -60,5 +77,34 @@ function update_check()
         }
     })
 
+    // Affichage compteur
     p.innerText = "Tache(s) " + compteur + " / " + max
+}
+
+// Fonction modifier tache
+function edit(div)
+{
+    // Récupere le li
+    const li = div.querySelector("li");
+
+    // Création zone de texte
+    const newText = document.createElement("input");
+    newText.type = "text";
+    newText.value = li.textContent;
+
+    // Création bouton de validation + eventListener
+    const saveButton = document.createElement("button");
+    saveButton.textContent = "Valider";
+    saveButton.addEventListener("click", function() {
+        if (newText.value) 
+        {
+            li.textContent = newText.value;
+            newText.replaceWith(li)
+            saveButton.remove();
+        }
+    });
+
+    // Remplace li par zone de texte et ajoute bouton validation
+    li.replaceWith(newText);
+    div.appendChild(saveButton);
 }
