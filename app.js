28493a2ec8b39d41ab588bce3f5408abd86d712c
Eden Vandewatyne <eden.vandewatyne@estiam.com>
2025-02-10 19:50:28 +0100

    Ajout du compteur et de sa maj

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const button_add = document.getElementById("add")
+const check = document.getElementById("checkbox")
+
+button_add.addEventListener("click", function() {
+    createDiv()
+    update_check()
+})
+
 function createDiv() {
     // Créer le div
     const div = document.createElement("div");
@@ -5,43 +13,52 @@ function createDiv() {
     // Créer l'élément li
     const li = document.createElement("li");
 
-    // recuperer valeur form
-    const name = document.getElementById("name").value
+    // Récupérer la valeur du formulaire
+    const name = document.getElementById("name").value;
 
-    //lier a li
+    // Lier à li
     li.textContent = name;
 
-    //creation checkbox
-    const checkbox = document.createElement("input")
-    checkbox.type = "checkbox"
+    // Création de la checkbox
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "checkbox";
 
-    // creer bouton supp
+    checkbox.addEventListener("change", function() {
+        update_check()
+    })
+
+    // Création du bouton de suppression
     const button = document.createElement("button");
     button.textContent = "Supprimer";
     button.addEventListener("click", function() {
         div.remove();
+        update_check();
     });
 
-
-    div.appendChild(checkbox)
+    div.appendChild(checkbox);
     div.appendChild(li);
     div.appendChild(button);
-
 
     document.getElementById("liste").appendChild(div);
 }
 
-
-
 function update_check()
 {
-    const liste = document.querySelectorAll("ul>input")
- 
+    const liste = document.querySelectorAll("ul > div > input")
+    const p = document.getElementById("compteur")
+
+    let compteur = 0
+    let max = 0
+
     liste.forEach(items => {
-        if(items.checked)
+        max += 1
+
+        if (items.checked)
         {
-            console.log("CheckBox Cliqué")
+            compteur += 1
         }
     })
+
+    p.innerText = "Tache(s) " + compteur + " / " + max
 }
- 
