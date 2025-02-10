a0e7f3e086eb934e96139490ebf60857068d6f7e
Eden Vandewatyne <eden.vandewatyne@estiam.com>
2025-02-10 18:34:06 +0100

    Checkbox

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const button = document.getElementById("add")
 button.addEventListener("click", function () {
     const ul = document.getElementById("liste")
     const li = document.createElement("li")
+    const checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
 
-    li.innerText = "Bonjour"
+    const name = document.getElementById("name").value
+
+    li.innerText = name
+
+    ul.append(checkbox)
     ul.append(li)
 })
